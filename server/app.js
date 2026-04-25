4bc16b5dd52fc17832875ef1df6379561dff6903
Niladri Sarkar <nsarkar2006@gmail.com>
2026-04-25 22:25:57 +0530

    Fixed route order in app.js and connected auth routes

--- server/app.js
+++ server/app.js
@@ -6,20 +6,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+
+// middleware
 app.use(express.json());
 
+// DB connection
+const connectDB = require("./config/db");
+connectDB();
+
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/cert", require("./routes/certificateRoutes"));
+
+// test route
 app.get("/", (req, res) => {
   res.send("Server Running 🚀");
 });
 
+// start server (ALWAYS LAST)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-const connectDB = require("./config/db");
-connectDB();
-
-app.use("/api/auth", require("./routes/authRoutes"));
-
-app.use("/api/cert", require("./routes/certificateRoutes"));
 
 console.log("ENV TEST:", process.env.MONGO_URI);
