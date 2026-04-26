fbb11010ac35bf33fb859553ffe8f45cc451af2d
Niladri Sarkar <nsarkar2006@gmail.com>
2026-04-27 03:04:41 +0530

    fixed certicateRoutes

--- server/routes/certificateRoutes.js
+++ server/routes/certificateRoutes.js
@@ -3,22 +3,6 @@ const router = express.Router();
 
 const upload = require("../middleware/upload");
 const authMiddleware = require("../middleware/authMiddleware");
-const { uploadCertificate } = require("../controllers/certificateController");
-
-// 🔐 Protected + file upload
-router.post(
-  "/upload",
-  authMiddleware,
-  upload.single("file"),
-  uploadCertificate
-);
-
-module.exports = router;
-const express = require("express");
-const router = express.Router();
-
-const upload = require("../middleware/upload");
-const authMiddleware = require("../middleware/authMiddleware");
 
 const {
   uploadCertificate,
