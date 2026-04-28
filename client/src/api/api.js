b8773c8c5816a9bc9d6bb5952aa57a1fd50eca8f
Niladri Sarkar <nsarkar2006@gmail.com>
2026-04-28 16:25:44 +0530

    added production backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://certchain-i5nw.onrender.com/api"
 });
 
 // AUTO ATTACH TOKEN
