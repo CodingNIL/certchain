5f0e1dd95412678b23fa73197316536e81aa71a8
Niladri Sarkar <nsarkar2006@gmail.com>
2026-04-28 16:09:30 +0530

    updated cors for deployment

--- server/app.js
+++ server/app.js
@@ -7,21 +7,30 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// DB
+// DATABASE
 connectDB();
 
+// CORS
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 // MIDDLEWARE
-app.use(cors());
 app.use(express.json());
 
 // ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/cert", require("./routes/certificateRoutes"));
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("CertChain Backend Running 🚀");
 });
 
+// SERVER
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
